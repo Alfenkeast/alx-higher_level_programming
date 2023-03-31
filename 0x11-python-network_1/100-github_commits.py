114300b77ce976c6f82d68910c5c73aa1705ca45
Alfenkeast <110989525+Alfenkeast@users.noreply.github.com>
2023-03-31 17:26:32 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""
-Python script that takes 2 arguments in order to solve this challenge.
-Usage: ./100-github_commits.py <repository name> <repository owner>
-"""
-from sys import argv
-import requests
+"""Python script that lists list 10 commits (from the most recent
+to oldest) of the repository “rails” by the user “rails”."""
 
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    url = "https://alx.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
-    req = requests.get(url)
-    commits = req.json()
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10"\
+          .format(argv[2], argv[1])
 
-    try:
-        for index in range(10):
-            print("{}: {}".format(
-                commits[index].get("sha"),
-                commits[index].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    request = requests.get(url)
+    commits = request.json()
+    for commit in commits:
+        print("{}: {}".format(commit.get("sha"),
+                              commit.get("commit").get("author").get("name")))
