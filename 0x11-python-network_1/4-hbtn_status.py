3f2213d48797b51b15664faf2574fde1b5ba0d64
Alfenkeast <110989525+Alfenkeast@users.noreply.github.com>
2023-03-31 16:36:04 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status
 """
+ fetches https://alx-intranet.hbtn.io/status
+"""
+
 import requests
 
-
-if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
-    print('tabulation before -')
-    print('\t- type:', type(r.text))
-    print('\t- content:', r.text)
+if __name__ == '__main__':
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
