f1ee7e2013fc7a19542e73fa045db0b5e0d182eb
Eke Nkechi <alfredpatience03@gmail.com>
2023-03-31 08:54:02 -0700

    correction

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    url = "https://alx.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     req = requests.get(url)
     commits = req.json()
 
